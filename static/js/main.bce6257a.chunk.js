42ce3f3bc2f3be817d40c5cc4794d528c0345927
Evan Hardek <evanhardek@gmail.com>
2021-03-09 23:14:33 -0500

    Updates

---
+++ static/js/main.bce6257a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpehch-react-employee-directory"]=this["webpackJsonpehch-react-employee-directory"]||[]).push([[0],{9:function(e,r,t){"use strict";t.r(r);t(1);var c=t(3),n=t.n(c),o=t(0);var i=function(){return Object(o.jsx)("h1",{children:"Employee Directory"})};var s=function(){return Object(o.jsx)(i,{})};n.a.render(Object(o.jsx)(s,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.bce6257a.chunk.js.map
